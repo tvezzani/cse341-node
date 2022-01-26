f8c975cea8a3def7f590da3be0bef2288f6f1daf
tvezzani <timv.red@gmail.com>
2022-01-26 14:45:07 -0700

    Starting model

--- MyServer/controllers/products.js
+++ MyServer/controllers/products.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product');
+const products = [];
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
@@ -11,20 +11,17 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
-  product.save();
+  products.push({ title: req.body.title });
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
   });
 };
